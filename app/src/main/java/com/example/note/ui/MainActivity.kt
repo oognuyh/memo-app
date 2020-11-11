790dba0450c2971a27a5dbf67a53843f020860ee
oognuyh <oognuyh@gmail.com>
2020-11-11 18:21:00 +0900

    implement db and ui

--- app/src/main/java/com/example/note/ui/MainActivity.kt
+++ app/src/main/java/com/example/note/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.note
+package com.example.note.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.note.R
 
 class MainActivity : AppCompatActivity() {
 
