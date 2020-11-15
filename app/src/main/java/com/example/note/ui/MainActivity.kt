0a66a15b862ffdb7838f9b3000d09e7adfcdf082
oognuyh <oognuyh@gmail.com>
2020-11-15 23:54:08 +0900

    implement ui

--- app/src/main/java/com/example/note/ui/MainActivity.kt
+++ app/src/main/java/com/example/note/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.note.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.note.R
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/note/ui/NoteFragment.kt
+++ app/src/main/java/com/example/note/ui/NoteFragment.kt
@@ -1,9 +1,7 @@
 package com.example.note.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.note.R
 
@@ -11,13 +9,11 @@ import com.example.note.R
  * 새 노트 작성 및 기존 노트 수정 기능
  */
 
-class NoteFragment: Fragment() {
+class NoteFragment: Fragment(R.layout.fragment_note) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
